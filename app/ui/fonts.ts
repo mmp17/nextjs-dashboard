cb53840df09ef90571980fdbc03b58a0409ac002
mmp17 <mirzaevmuzaffar@yahoo.com>
2024-05-04 07:23:35 +0900

    feat: optimizing images disktop and mob

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const roboto_mono = Lusitana({
+export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
