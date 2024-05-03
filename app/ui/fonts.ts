6b387b2ac1e983e151b57b30ac84cfb982758073
mmp17 <mirzaevmuzaffar@yahoo.com>
2024-05-04 06:58:59 +0900

    BRR: initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const roboto_mono = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
